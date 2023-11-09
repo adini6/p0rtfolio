2063a6e78f81017a1d06d7181925e3ed74168ee0
Abdikalik Dini <adini18@gmail.com>
2023-11-09 18:39:52 -0500

    added components about, project and contact and started about section

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"
+import About from "./components/About";
+import Projects from "./components/Project";
+import Contact from "./components/Contact";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex min-h-screen bg-black text-white">
+      
+    <div className="w-64 fixed inset-y-0 left-0 p-8">
+      <h1 className="text-4xl font-bold">Abdikaliik Dini</h1>
+      <p className="mt-4">Full Stack Developer</p>
+      
+      <nav className="mt-8">
+        <a href="#about" className="block mt-2">About</a>
+        <a href="#projects" className="block mt-2">Projects</a>
+        <a href="#contact" className="block mt-2">Contact</a>
+
+      </nav>
     </div>
-  );
+    
+    <div className="flex-1 p-8 ml-64 overflow-y-auto">
+      <section id="about" className="pt-8">
+        <About/>
+      </section>
+      <section id="projects" className="pt-8">
+        <Projects/>
+      </section>
+      <section id="contact" className="pt-8">
+        <Contact/>
+      </section>
+    </div>
+  </div>
+);
 }
 
 export default App;
---
+++ portfolio/src/components/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function About() {
+    return (
+        <div className="bg-black text-white p-10 flex items-center justify-center min-h-screen">
+            <div className="container mx-auto max-w-2xl">
+                <h1 className="text-5xl font-bold mb-6 text-center">About</h1>
+                <p className="text-xl leading-relaxed text-center">
+                    Hi! My name is Dini, I am a developer that enjoys projecting what's on my mind and placing it on a screen near you. I enjoy the never-ending aspect of learning and the challenges that come with being a full-stack developer.
+                </p>
+            </div>
+        </div>
+    );
+}
+
+export default About;
